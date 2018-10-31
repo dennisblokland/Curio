efa3ab7b8b205a328f2f859d47ed042a09fe84d5
Unknown <denbukki@gmail.com>
2018-10-31 16:07:54 +0100

    added sub items to mysticcrystal

--- src/main/java/com/denbukki/baublelicious2/items/ItemMysticCrystal.java
+++ src/main/java/com/denbukki/baublelicious2/items/ItemMysticCrystal.java
@@ -1,7 +1,13 @@
 package com.denbukki.baublelicious2.items;
 
+import com.denbukki.baublelicious2.Baublelicious2;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -13,12 +19,46 @@ public class ItemMysticCrystal extends ItemBase{
 
     public ItemMysticCrystal(){
         super("ItemMystic_Crystal");
+        this.setHasSubtypes(true);
+        setMaxDamage(0);
+
     }
+    public static final int[] Levels = new int[] {0, 5, 10, 15, 20, 25, 30, 35, 50, 100};
 
     @SideOnly(Side.CLIENT)
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
     {
-        tooltip.add("The Crystal seems to have weak mystic properties");
+        if(this.getDamage(stack) == 0){
+            tooltip.add("The Crystal seems to have weak mystic properties");
+        }else{
+            tooltip.add("Level " + this.getDamage(stack)  );
+        }
+
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack stack)
+    {
+        return this.getDamage(stack) != 0;
+    }
+
+    public String getUnlocalizedName(ItemStack stack)
+    {
+
+        return super.getUnlocalizedName();
+    }
+
+    public void registerItemModel() {
+        for (int level : Levels) {
+            Baublelicious2.proxy.registerItemRenderer(this, level, name);
+        }
+    }
+
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+        for (int level : Levels) {
+            items.add(new ItemStack(this, 1, level));
+
+        }
     }
 }
